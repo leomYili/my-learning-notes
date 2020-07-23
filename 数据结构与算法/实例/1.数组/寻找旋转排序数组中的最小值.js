b6bad4a4fc6f671b3c629b7d7b5c44a2db1fc9fe
leomYili <leomy@outlook.com>
2020-07-23 10:10:41 +0800

    feat: 增加前端监控体系资料整理与分析，7.22日算法题打卡

---
+++ 数据结构与算法/实例/1.数组/寻找旋转排序数组中的最小值.js
@@ -0,0 +1,39 @@
+// 输入[1,3,5];输出1
+// 输入[2,2,2,0,1];输出0
+
+/**
+ * 该方法为 O(n)
+ * @param {} nums 
+ */
+var findMin = function(nums){
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] > nums[i+1]){
+            return nums[i + 1];
+        }
+    }
+
+    return nums[0];
+}
+
+/**
+ * 二分法，对数组取中间数，然后依次与数组当前最后一位（相等时往前移位）进行比较，得到最小值下标
+ * @param {*} nums 
+ */
+var findMin = function(nums){
+    var low = 0;
+    var high = nums.length - 1;
+
+    while(low < high){
+        var mid = (low + high) >> 1;
+        
+        if(nums[mid] > nums[hight]){
+            low = mid + 1;
+        }else if(nums[mid] == nums[high]){
+            high--;
+        }else{
+            high = mid;
+        }
+    }
+
+    return nums[low];
+}
